989a9bc117ab5a5586c1ca8c903bed14933a19cd
Mikhail Voloshin <mvol@mightydatainc.com>
2023-03-12 19:09:14 -0400

    Getting the structure of my project into place.

--- www/src/App.vue
+++ www/src/App.vue
@@ -10,12 +10,37 @@ onMounted(() => {});
   <Header></Header>
 
   <main>
-    <div class="mainblock"></div>
+    <div class="interactive-panels mainblock">
+      <div class="interactive-panel interactive-panels--nextword"></div>
+      <div class="interactive-panel interactive-panels--embeddingdistance"></div>
+    </div>
   </main>
 </template>
 
 <style scoped lang="scss">
 .mainblock {
   margin: auto;
+}
+
+.interactive-panels {
+  display: flex;
+  flex-direction: row;
+
+  margin: 1em;
+
+  @media screen and (max-width: 960px) {
+    display: block;
+    max-width: 640px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .interactive-panel {
+    flex: 1;
+    margin: 1em;
+    margin-top: 0;
+    text-align: center;
+    min-width: 16em;
+  }
 }
 </style>
