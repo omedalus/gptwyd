4c1a9f09ada139454f6eeead44d92c18dc024b81
Mikhail Voloshin <mvol@mightydatainc.com>
2023-03-12 16:44:51 -0400

    Got my linters how I like em

--- www/.eslintrc.cjs
+++ www/.eslintrc.cjs
@@ -1,15 +1,20 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/no-mutating-props': 0,
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/no-unused-vars': 0
   }
-}
+};
