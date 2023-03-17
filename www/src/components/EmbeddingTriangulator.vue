8160e596cda9ed1f852a8c358938865f30f8cbdf
Mikhail Voloshin <mvol@mightydatainc.com>
2023-03-16 23:54:27 -0400

    Embeddings vis works well. Right now it just outputs numbers, but it's almost ready for colored graphs.

--- www/src/components/EmbeddingTriangulator.vue
+++ www/src/components/EmbeddingTriangulator.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
 
 import type { OpenAIApi } from 'openai';
 
@@ -14,6 +14,27 @@ const elemVariableInput = ref();
 
 const embeddingRefLeft = ref(null as number[] | null);
 const embeddingRefRight = ref(null as number[] | null);
+
+const _dot = (v1: number[] | null, v2: number[] | null) => {
+  if (!v1 || !v2) {
+    return null;
+  }
+  if (v1.length !== v2.length) {
+    return null;
+  }
+  let retval = 0;
+  for (let i = 0; i < v1.length; i++) {
+    retval += v1[i] * v2[i];
+  }
+
+  // Normalize to a realistic range, because embeddings from GPT
+  // are in such a high dimensional space that it's practically impossible
+  // to find an actual orthogonal pair.
+  retval = (retval - 0.7) / (1 - 0.7);
+  retval = Math.max(retval, 0);
+
+  return retval;
+};
 
 const _getValueFromEventTarget = (ev: Event) => {
   const retval = (ev.target as HTMLInputElement)?.value || '';
@@ -43,6 +64,40 @@ watch(textRefLeft, async (newval) => {
 watch(textRefRight, async (newval) => {
   embeddingRefRight.value = await _getEmbeddingVectorForText(newval);
 });
+
+const refComparison = computed(() => {
+  return _dot(embeddingRefLeft.value, embeddingRefRight.value);
+});
+
+const colorSimilarity = (similarity: number) => {
+  const r = similarity > 0.5 ? 2 * (1 - similarity) : 1;
+  const g = similarity < 0.5 ? 2 * similarity : 1;
+  const retval = `rgb(${Math.floor(r * 255)}, ${Math.floor(g * 255)}, 0)`;
+  return retval;
+};
+
+type VariableText = {
+  text: string;
+  embedding: number[] | null;
+};
+
+const updateStupidVizTrick = ref(0);
+
+const variableTexts = ref(Array<VariableText>());
+
+const onVariableTextSubmitted = async (text: string) => {
+  text = (text || '').replace('\n', ' ').trim();
+  if (!text) {
+    return;
+  }
+  const newVT = {
+    text,
+    embedding: null
+  } as VariableText;
+  variableTexts.value.push(newVT);
+  newVT.embedding = await _getEmbeddingVectorForText(text);
+  updateStupidVizTrick.value = Math.random();
+};
 </script>
 
 <template>
@@ -53,7 +108,8 @@ watch(textRefRight, async (newval) => {
       <div class="refinput-instructions">
         <p>
           Enter a pair of "reference texts". These are two short pieces of text &mdash; each a
-          phrase or a sentence.
+          phrase or a sentence. Then, enter a "variable text" and press the button to compare it to
+          both reference texts.
         </p>
       </div>
       <div class="refinput-fields">
@@ -64,7 +120,16 @@ watch(textRefRight, async (newval) => {
           placeholder='Enter a phrase or sentence for the Left Reference Text, e.g. "The cat sat on the rug."'
         ></textarea>
         <div class="refinput-affinities">
-          <div>These two texts have a conceptual similarity score of</div>
+          <div v-if="refComparison !== null">
+            <div style="padding: 0 1ex">These two texts have a conceptual similarity score of:</div>
+
+            <div
+              class="similarity-bar"
+              :style="{ backgroundColor: colorSimilarity(refComparison) }"
+            >
+              {{ (refComparison * 100).toFixed(2) }}%
+            </div>
+          </div>
         </div>
         <textarea
           style="border-right: none"
@@ -78,17 +143,36 @@ watch(textRefRight, async (newval) => {
           ref="elemVariableInput"
           placeholder='Enter a "Variable Text". We&apos;ll compare this text to the two Reference Texts.'
         ></textarea>
-        <div class="variable-input-button gptwyd-btn">
+        <div
+          class="variable-input-button gptwyd-btn"
+          @click="
+            onVariableTextSubmitted(elemVariableInput.value);
+            elemVariableInput.value = '';
+          "
+        >
           🡇 Compare this text to the two references 🡇
         </div>
       </div>
     </div>
 
     <div class="triangulated-variables">
-      TRIANGLE MAN
-
-      {{ textRefLeft }}
-      {{ textRefRight }}
+      <div class="triangulated-list">
+        <div class="triangulated-list-item" v-for="vt in variableTexts" :key="vt.text">
+          <div class="triangulated-list-item-text">
+            {{ vt.text }}
+          </div>
+          <div class="triangulated-similarity-bar-holder">
+            <div
+              v-if="embeddingRefLeft && embeddingRefRight && vt.embedding && updateStupidVizTrick"
+            >
+              <div class="triangulated-similarity-bar">
+                Left: {{ _dot(embeddingRefLeft, vt.embedding) }} Right:
+                {{ _dot(embeddingRefRight, vt.embedding) }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="explanation explanation-what-is-embedding">
@@ -141,11 +225,10 @@ watch(textRefRight, async (newval) => {
     .refinput-fields {
       display: flex;
       flex-direction: row;
-      gap: 1em;
 
       & > textarea {
         flex: 1;
-        height: 5em;
+        height: 6em;
         resize: none;
       }
 
@@ -173,6 +256,40 @@ watch(textRefRight, async (newval) => {
 
   .variable-input-button {
     display: inline-block;
+  }
+
+  .similarity-bar {
+    border-top: 1px solid #cc6;
+    border-bottom: 1px solid #cc6;
+    font-weight: bold;
+  }
+
+  .triangulated-list-item {
+    margin-top: 1em;
+    padding-top: 1ex;
+    padding-bottom: 1ex;
+    border: 1px solid #886;
+    border-left: none;
+    border-right: none;
+    border-bottom: none;
+
+    .triangulated-list-item-text {
+      display: inline-block;
+      font-style: italic;
+
+      &::before {
+        content: '“';
+      }
+
+      &::after {
+        content: '”';
+      }
+    }
+
+    .triangulated-similarity-bar-holder {
+      height: 1.5em;
+      background-color: #eee;
+    }
   }
 }
 </style>
