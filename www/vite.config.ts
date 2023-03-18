68aa6eb4ec98406375976557bc4d50284fd32311
Mikhail Voloshin (desktop workstation) <mvol@live.com>
2023-03-18 11:05:07 -0400

    Set high chunk size warning limit

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    // Set a ridiculously high chunk size just to shut it up.
+    chunkSizeWarningLimit: 10 * 1000 * 1000
   }
-})
+});
